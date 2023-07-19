6e64cf14341f2042758b7278d94f284c8d51f387
Evanildo Silva <evanildo.sf91@gmail.com>
2023-07-19 15:50:39 -0300

    feat: cors

--- src/config/server.js
+++ src/config/server.js
@@ -3,9 +3,11 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express() // startar o express - criar instância do express.
+const allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extended: true })) // midleware
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function () {
   console.log(`BACKEND is running on port ${port}`)
